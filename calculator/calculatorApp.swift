7835f2c634b2d0a0d0b1ceb2523a0cc1d87ec451
Tereza <tereza.chodurova@gmail.com>
2023-03-21 16:33:47 +0100

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Terka on 21.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
